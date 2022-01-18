9b389684e5b49b93d49d45d6554bfa0b85376f75
Upendra <bhadauriaupendra062@gmail.com>
2022-01-18 12:10:46 +0530

    initial commit

---
+++ upendra53.js
@@ -0,0 +1,30 @@
+console.log("This is learning java script by rocky sir");
+
+function great(name , greatText="Greetings from JavaScript"){
+// function great(name , greatText){
+    console.log(greatText + " " + name)
+    console.log(name + " is a good boy");
+}
+
+function sum(a, b ,c){
+    let d = a + b + c;
+    return d;
+}
+
+let name = "Rocky";
+let name1 = "upendra";
+let name2 = "jetu";
+let name3 = "rishi";
+let greatText = "Good Morning"
+
+// great(name , greatText);
+// great(name1 , greatText);
+// great(name2, greatText);
+// great(name3, greatText);
+// great(name3);
+let returnVal = sum(1,2,3);
+console.log(returnVal)
+// console.log(name + " is a good boy");
+// console.log(name1 + " is a good boy");
+// console.log(name2 + " is a good boy");
+// console.log(name3 + " is a good boy");
